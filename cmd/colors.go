156650d0b274a74f59b5800fabb53b5733519ae2
Ege Güneş <egegunes@gmail.com>
2019-12-15 21:32:27 +0300

    Add deprecation notice to README

--- cmd/colors.go
+++
@@ -1,6 +0,0 @@
-package cmd
-
-import "github.com/fatih/color"
-
-var GREEN = color.New(color.FgGreen).SprintFunc()
-var RED = color.New(color.FgRed).SprintFunc()
--- main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/egegunes/gitlabenv/cmd"
-
-func main() {
-	cmd.Execute()
-}
